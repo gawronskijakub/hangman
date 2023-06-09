d58c22d9e7f1bceead5cab546e7eed86057e3d4f
Jakub <44299334+gawronskijakub@users.noreply.github.com>
2023-06-09 20:18:11 +0200

    Added basic file structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,9 @@
+import { Main } from '../Main';
+
 export const App = (): JSX.Element => {
-  return <div className='app'>app</div>
+	return (
+		<div className='app'>
+			<Main />
+		</div>
+	);
 };
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,3 @@
+export const Main = (): JSX.Element => {
+	return <main className='main'>main</main>;
+};
