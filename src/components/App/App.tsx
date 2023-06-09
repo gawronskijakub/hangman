4984f549d39b48876ede1fb7224d1f275aecfd39
Jakub <44299334+gawronskijakub@users.noreply.github.com>
2023-06-09 20:32:53 +0200

    Setup (#1)

---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+import { Main } from '@/components/Main';
+
+export const App = (): JSX.Element => {
+	return (
+		<div className='app'>
+			<Main />
+		</div>
+	);
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/hangman/'
-})
+	plugins: [react()],
+	base: '/hangman/',
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src/'),
+		},
+	},
+});
