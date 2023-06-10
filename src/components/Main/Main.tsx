1e6d30abd3c1a0c13402d8135504fe6cc15e15f9
Jakub <44299334+gawronskijakub@users.noreply.github.com>
2023-06-10 19:13:51 +0200

    Added letter guessing functionality

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,3 +1,35 @@
+import { KeyboardEvent, useState } from 'react';
+
+import styles from './Main.module.scss';
+
 export const Main = (): JSX.Element => {
-	return <main className='main'>main</main>;
+	const wordToGuess = 'alphabet';
+	const [word, setWord] = useState('_'.repeat(wordToGuess.length));
+
+	const printLetter = (ev: KeyboardEvent) => {
+		const keyPressed = ev.key;
+
+		if (!wordToGuess.includes(keyPressed)) return;
+
+		const indices = [...wordToGuess].map((el, index) => (el === keyPressed ? index : '')).filter(String);
+
+		setWord((currentWord) => {
+			indices.forEach((index) => {
+				currentWord = currentWord.replace(/./g, (char, currIndex) => (currIndex === index ? keyPressed : char));
+			});
+
+			return currentWord;
+		});
+	};
+
+	return (
+		<main
+			className={styles.main}
+			tabIndex={-1}
+			onKeyDown={printLetter}
+		>
+			<p>{word}</p>
+			<p>{!word.includes('_') && 'BRAVO'}</p>
+		</main>
+	);
 };
